0fa4926e0737aa1ef4accded46a8ad7e1e8b0184
Gerard Konings <gerard@youngdavid.nl>
2017-09-27 17:51:40 +0200

    refactor grid -> layout

--- src/views/admin/ManageUnits.jsx
+++ src/views/admin/ManageUnits.jsx
@@ -4,8 +4,8 @@ import Header from 'components/page/header'
 import Form from 'components/units/form'
 
 import { Unit, Units } from '../../blocks'
-import Grid from '../../components/grid/Grid'
-import Content from '../../components/grid/Content'
+import Grid from '../../components/layout/Grid'
+import Content from '../../components/layout/Content'
 
 const ManageUnits = ({manageUnits, ...props}) => {
   return (
