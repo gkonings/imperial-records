8f3168a1899cfdc8c17f8f7b0ff8433fc8c9c347
Gerard Konings <gerard@youngdavid.nl>
2017-09-29 17:57:18 +0200

    userphoto

--- src/components/page/header/Portrait.jsx
+++ src/components/page/header/Portrait.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import styles from './Portrait.module.scss'
+
 const Portrait = ({user}) => {
+  console.log(user)
+  const background = {
+    backgroundImage: `url('${user.photoURL}')`
+  }
   return (
-    <div>
-      <img src={user.photoUrl} alt={user.displayName} />
-    </div>
+    <div className={styles.portrait}
+      style={background} />
   )
 }
 
--- src/components/page/header/UserMenu.jsx
+++ src/components/page/header/UserMenu.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 import Login from './Login'
 import Portrait from './Portrait'
+import styles from './UserMenu.module.scss'
 
 const UserMenu = ({admin, user, ...props}) => {
-  console.log(user)
-
   return (
-    <div>
-      {user &&
-      <Portrait user={user} />}
-      {admin &&
+    <div className={styles.userMenu}>
+
+      <div className={styles.login}>
+        {admin &&
         <Login
           loggedIn={user}
           login={props.login}
           logout={props.logout} />}
+      </div>
+      <div className={styles.portrait}>
+        {user &&
+        <Portrait user={user} />}
+      </div>
     </div>
   )
 }
