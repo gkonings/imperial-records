eb9e7a763e7f0ad762fca5531b5fa1ac0df42549
Gerard Konings <>
2017-10-15 09:38:18 +0200

    resizeUrl bug

--- src/services/images/resizeUrl.js
+++ src/services/images/resizeUrl.js
@@ -1,5 +1,5 @@
 const resizeUrl = (url, size) => {
-  if (!size) return url
+  if (!url || !size) return url
 
   const index = url.indexOf('/photo.')
   const newUrl = url.slice(0, index) + `/s${size}-c` + url.slice(index)
--- src/services/images/resizeUrl.spec.js
+++ src/services/images/resizeUrl.spec.js
@@ -1,16 +1,32 @@
 /* eslint-env jest */
 import resizeUrl from './resizeUrl'
 
+const correctInput = 'https://lh3.googleusercontent.com/-7bWe5X_yvmU/AAAAAAAAAAI/AAAAAAAAEYk/GbQmHxgQo9o/photo.jpg'
+const correctOutput = 'https://lh3.googleusercontent.com/-7bWe5X_yvmU/AAAAAAAAAAI/AAAAAAAAEYk/GbQmHxgQo9o/s80-c/photo.jpg'
+
+const incorrectInput = 'someOtherString'
+
 test('returns same string without param', () => {
-  const input = 'https://lh3.googleusercontent.com/-7bWe5X_yvmU/AAAAAAAAAAI/AAAAAAAAEYk/GbQmHxgQo9o/photo.jpg'
-  const output = resizeUrl(input)
-  expect(output).toBe(input)
+  const output = resizeUrl(correctInput)
+  expect(output).toBe(correctInput)
+})
+
+test('returns same string with incorrectInput', () => {
+  const output = resizeUrl(incorrectInput)
+  expect(output).toBe(incorrectInput)
+})
+
+test('returns undefined with undefined input', () => {
+  const output = resizeUrl()
+  expect(output).toBe(undefined)
+})
+
+test('returns undefined with undefined input(80)', () => {
+  const output = resizeUrl(undefined, 80)
+  expect(output).toBe(undefined)
 })
 
 test('returns a resized string (80)', () => {
-  const input = 'https://lh3.googleusercontent.com/-7bWe5X_yvmU/AAAAAAAAAAI/AAAAAAAAEYk/GbQmHxgQo9o/photo.jpg'
-  const output = resizeUrl(input, 80)
-  const expected = 'https://lh3.googleusercontent.com/-7bWe5X_yvmU/AAAAAAAAAAI/AAAAAAAAEYk/GbQmHxgQo9o/s80-c/photo.jpg'
-
-  expect(output).toBe(expected)
+  const output = resizeUrl(correctInput, 80)
+  expect(output).toBe(correctOutput)
 })
